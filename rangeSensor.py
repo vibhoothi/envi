664faaefb7e1af92c34d2c0e431736753901fdc1
Vibhoothi <vibhoothiiaanand@gmail.com>
2018-10-12 21:45:11 +0530

    rangeSensor.py: Add distance mesurement  logic
    This commit  completes the implementation of distance mesurement with voice
    feedback for the raspberry Pi
    
    Authors:  Mayoogh Girish <mayoogh1997@gmail.com>
    	  Vibhoothi <vibhoothiiaanand@gmail.com>
    	  Anirudh Mohan TP <imtpmrk7@gmail.com>

--- rangeSensor.py
+++ rangeSensor.py
@@ -6,5 +6,36 @@ GPIO.setmode(GPIO.BCM)
 TRIG = 9
 ECHO = 10 
 
+while True:
+  a=100
+  engine = pyttsx.init()
+ 
+ print "Distance Measurement In Progress"
+ GPIO.setwarnings(False)
+ GPIO.setup(TRIG,GPIO.OUT)
+ GPIO.setup(ECHO,GPIO.IN) 
+ GPIO.output(TRIG, False)
+ print "Waiting For Sensor To Settle"
+ time.sleep(.5)
+
+ GPIO.output(TRIG, True)
+ time.sleep(0.00001)
+ GPIO.output(TRIG, False)
+ start = time.time() 
+
+ while GPIO.input(ECHO)==0:
+   start = time.time()
+
+ while GPIO.input(ECHO)==1:
+   end = time.time()
+ duration = end - start
+ distance = duration * 17150
+ distance = round(distance, 2)
+
+ print "Distance:",distance,"cm"
+
+ if distance<=a: 
+  engine.say("Stop,Obstacle ahead at (%d) centimeters"%distance)
+  engine.runAndWait()
 
 GPIO.cleanup()
