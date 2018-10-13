1729658854428e979e9edf9c95d8a891f627290a
Vibhoothi <vibhoothiiaanand@gmail.com>
2018-10-13 17:32:50 +0530

    stepDownShoe.ino: Complete the implementation of stepDownShoe
    This commit includes detection of pitfall, stepdown, and elevation change which
    gives vibration at a diffrent levels for user for a nice userExperience
    Authors:  Mayoogh Girish <mayoogh1997@gmail.com>
              Vibhoothi <vibhoothiiaanand@gmail.com>
              Anirudh Mohan TP <imtpmrk7@gmail.com>

--- stepDownShoe.ino
+++ stepDownShoe.ino
@@ -14,4 +14,66 @@ void setup()
 float distance1, distance2, duration1, duration2 ;
 void loop() 
 {
+/*
+ * Sensor 1
+*/
+        digitalWrite(trig1,LOW);   
+        delay(2);
+        digitalWrite(trig1,HIGH);
+        delay(10);
+        digitalWrite(trig1,LOW);
+        duration1 =pulseIn(echo1,HIGH);
+        distance1 = (duration1/2)/29.1;
+        Serial.print(distance1);
+        Serial.println();
+        delay(10);
+/*
+ * Sensor 1
+*/
+        digitalWrite(trig2,LOW);   
+        delay(2);
+        digitalWrite(trig2,HIGH);
+        delay(10);
+        digitalWrite(trig2,LOW);
+        duration2 =pulseIn(echo2,HIGH);
+        distance2 = (duration2/2)/29.1;
+        Serial.print(distance2);
+        Serial.println();
+        delay(10);
+
+   if(distance1>8 && distance1<20)
+   {
+        if( distance1-distance2<1 && distance1-distance2>-1)
+        {
+                Serial.print("Its a step down");
+                Serial.println();
+                digitalWrite(vib,HIGH);
+                delay(2500); 
+                digitalWrite(vib,LOW);
+        }
+        else
+        {
+                Serial.print("Its a pitfall");
+                Serial.println();
+                digitalWrite(vib,HIGH);
+                delay(200);
+                digitalWrite(vib,LOW);
+  
+        }   
+   }
+   else if(distance1>20)
+   {
+        Serial.print("Its a pitfall");
+        Serial.println();
+        digitalWrite(vib,HIGH);
+        delay(200);
+        digitalWrite(vib,LOW);
+   }
+   else  
+   {
+        Serial.print("Its just an elevation change");
+        Serial.println();
+        digitalWrite(vib,LOW);
+        delay(200);
+   } 
 }
