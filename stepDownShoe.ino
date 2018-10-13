1f18f5442bea40f20cab477ea778438e03879704
Vibhoothi <vibhoothiiaanand@gmail.com>
2018-10-13 17:31:42 +0530

    stepDownShoe.ino:Initialse stepDownShoe for future implemenation by adding basic layout
    Authors:  Mayoogh Girish <mayoogh1997@gmail.com>
              Vibhoothi <vibhoothiiaanand@gmail.com>
              Anirudh Mohan TP <imtpmrk7@gmail.com>

---
+++ stepDownShoe.ino
@@ -0,0 +1,17 @@
+int trig2=11,echo2=10 ;
+int trig1=9,echo1=8 ;   
+int vib=13;
+ 
+void setup()
+{ 
+  pinMode(vib,OUTPUT);
+  pinMode(trig1,OUTPUT);
+  pinMode(echo1,INPUT);
+  pinMode(trig2,OUTPUT);
+  pinMode(echo2,INPUT);
+  Serial.begin(9600);
+}
+float distance1, distance2, duration1, duration2 ;
+void loop() 
+{
+}
